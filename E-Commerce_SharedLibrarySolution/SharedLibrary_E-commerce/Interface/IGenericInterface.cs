439277c2b64160ddc0427a73e5ea1675c8f77284
Juan4911 <jagutierrez451@gmail.com>
2024-10-06 22:49:00 -0500

    Shared Library

---
+++ E-Commerce_SharedLibrarySolution/SharedLibrary_E-commerce/Interface/IGenericInterface.cs
@@ -0,0 +1,15 @@
+﻿using SharedLibrary_E_commerce.Responses;
+using System.Linq.Expressions;
+
+namespace SharedLibrary_E_commerce.Interface
+{
+    public interface IGenericInterface<T> where T : class
+    {
+        Task<Response> CreateAsync(T entity);
+        Task<Response> UpdateAsync(T entity);
+        Task<Response> DeleteAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> FindByIdAsync(int id);
+        Task<T> GetByAsync(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ E-Commerce_SharedLibrarySolution/SharedLibrary_E-commerce/Responses/Response.cs
@@ -0,0 +1,4 @@
+﻿namespace SharedLibrary_E_commerce.Responses
+{
+    public record Response(bool Flag =false, string Message= null!);    
+}
